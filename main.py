ba33d59578a0686434b1a9807045d53fba07ca14
enotick1337 <101464958+enotick1337@users.noreply.github.com>
2022-12-01 22:20:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import random
-password = list('abcdefghijklmnopqrstuvwxyz')
-password = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
-password = list('1234567890')
-random.shuffle(password)
-password = ''.join([random.choice(password) for x in range(11)])
-print (password)
+import random 
+
+def password():
+    s1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ]
+    s2 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' ]
+    result = ''
+    for i in range(11):
+    	result += random.choice(s1+s2)
+    return result
+
+p = password()
+print(p)
