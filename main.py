16f938c25375e6b5a682d36f2dc3709e7e3d0e7f
Artem <enotick1337@mail.ru>
2022-12-01 15:47:44 +0300

    Модернизировали main.py на ветке hotfix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 password = list('abcdefghijklmnopqrstuvwxyz')
+password = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 random.shuffle(password)
 password = ''.join([random.choice(password) for x in range(11)])
 print (password)
